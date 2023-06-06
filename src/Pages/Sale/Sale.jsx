b18d985a8a7cc8813f0ef136a46fac8ca0e178dd
labchuk <labchuks70@gmail.com>
2023-06-06 23:19:13 +0300

    ladd3

--- src/Pages/Sale/Sale.jsx
+++ src/Pages/Sale/Sale.jsx
@@ -8,18 +8,18 @@ const Sale = () => {
     const {panties} = useSelector(state => state)
     const dispatch = useDispatch()
     useEffect(()=>{
-        if (panties.length === 0) dispatch(fetchPanties())
+        if (panties?.length === 0) dispatch(fetchPanties())
 
     },[])
     return (
         <>
             <div className={classes.sale}>
-                <h1 className={classes.mainTitle}>Акція - 5 трусиків за {(panties[0].price - panties[0].discount) * 5} грн</h1>
+                <h1 className={classes.mainTitle}>Акція - 5 трусиків за {(panties[0]?.price - panties[0]?.discount) * 5} грн</h1>
                 <p className={classes.mainSubTitle}>за умовами акції колір обираємо ми</p>
             </div>
             <Cards panties={panties}/>
         </>
-
+veve
     );
 };
 
